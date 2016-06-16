c0efc7f457fc6e2e79efc12d9d6012debd7f6d85
Davi Pereira <xdavipereira@gmail.com>
2016-06-16 02:43:25 +0000

    Fix bug in the delete test

--- spec/features/deleting_projects_spec.rb
+++ spec/features/deleting_projects_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
     RSpec.feature "Users can delete projects" do
     scenario "successfully" do
+                FactoryGirl.create(:project, name: "Sublime Text 3")
         visit "/"
         click_link "Sublime Text 3"
         click_link "Delete Project"
