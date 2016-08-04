1c6ca49a630e11faf6f1e560840edf6f2c4bd413
Davi Pereira <xdavipereira@gmail.com>
2016-08-04 20:55:37 -0300

    add sign in and sign out

---
+++ spec/features/signing_out_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.feature "User can signing out" do
+
+	let!(:user) { FactoryGirl.create(:user) }
+
+	before do 
+		login_as(user)
+	end
+
+
+	scenario do
+		visit "/"
+		click_link "Sign out"
+		expect(page).to have_content "Signed out successfully."
+	end
+
+end
