b0f18cc45e032b084dfb7defa56806b4a7434689
Davi Pereira <xdavipereira@gmail.com>
2016-08-25 19:49:49 -0300

    Add feature for admin can  archiving users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+	
+	def archive
+		self.update(archived_at: Time.now)
+	end
 
+	scope :excluding_archived, lambda { where(archived_at: nil)}
 
 	def to_s
 		"#{email} (#{admin? ? "Admin" : "User"})"
