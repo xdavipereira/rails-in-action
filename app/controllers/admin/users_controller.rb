e0c44d3c2fd0c79a5499d37e59325a58ecf42559
Davi Pereira <xdavipereira@gmail.com>
2016-08-25 17:15:43 -0300

    Add ability for admins edit and update users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,7 @@
 class Admin::UsersController < Admin::ApplicationController
+
+  before_action :set_user, only: [:show,:edit,:update,:destroy]
+
   def index
     @users  = User.order(:email)
   end
@@ -7,23 +10,47 @@ class Admin::UsersController < Admin::ApplicationController
     @user = User.new
   end
 
-  def create
-  	@user = User.new(user_params)
-
-	 if @user.save
-	flash[:notice] = "User has been created."
-	redirect_to admin_users_path
-	else
-	flash.now[:alert] = "User has not been created."
-	render "new"
-	end
+  def show
   end
 
+  def edit
+  end
 
+  def update
+    
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+    end
+
+    if @user.update(user_params)
+      flash[:notice] = "User has been updated."
+      redirect_to admin_users_path
+    else
+      flash.now[:alert] = "User has not been updated."
+      render "edit"
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "User has been created."
+      redirect_to admin_users_path
+    else
+      flash.now[:alert] = "User has not been created."
+      render "new"
+    end
+  end
 
   private
+
   def user_params
     params.require(:user).permit(:email, :password, :admin)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
